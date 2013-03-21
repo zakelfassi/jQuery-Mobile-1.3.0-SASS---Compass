b64640c553fc027b6d3c5e550807b6117b3aaf26
Zak Elfassi <zakelfassi@gmail.com>
2013-03-21 10:53:50 +0000

    - Change .gitignore. - Delete compiled CSS files. - Delete unused jqm files.

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
 
-output_style = :expanded
+output_style = :compressed
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
