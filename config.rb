d87a374633d62e5db866d0ad1e929d71f247b8b1
Zak Elfassi <zakelfassi@gmail.com>
2013-03-29 21:05:48 +0000

    - Add image folder. - Fix bug#1 reported by @uglow - Compile SASS to :expanded. - Delete unused config.rb files.

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
 
-output_style = :compressed
+output_style = :expanded
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
